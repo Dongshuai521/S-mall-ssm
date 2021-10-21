1bfdd8c3c25b5b834d98b1a7b082cfbb657078ec
Dong Shuai <1821511898@qq.com>
2021-10-21 23:58:11 +0800

    test

--- src/main/java/tmall/test/ProductTest.java
+++ src/main/java/tmall/test/ProductTest.java
@@ -25,6 +25,7 @@ public class ProductTest extends AbstractJUnit4SpringContextTests {
     public void getProduct() throws Exception {
         productService.get(5);
         System.out.println("test");
+        //test
         BeanFactory bf = new XmlBeanFactory(new ClassPathResource(""));
     }
 }
